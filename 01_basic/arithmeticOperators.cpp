55ea74cc89d93be3bc8f65baf5b0645a769a8c91
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-05 07:40:34 +0000

    arithmetic operators (+ - * / %)

---
+++ 01_basic/arithmeticOperators.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 8;
+    int y = 5;
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x/y<<endl;
+    cout<<x*y<<endl;    
+    cout<<x%y<<endl; 
+    
+    //increment and decrement
+
+    int n = 9;
+    cout<<n<<endl;
+    n++;    //post increment
+    cout<<n<<endl;
+
+    ++n ;    //pre increment
+    cout<<n<<endl;
+
+}
