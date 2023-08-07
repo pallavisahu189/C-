683827c725d33daa97c5f349b953a0cab8c7242f
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-07 09:20:32 +0000

    Find Remainder

---
+++ 01_basic/findRemainder.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a = 14;   //a means dividend
+    // int b = 3;     // b is division
+    // int q = a/b;  // q is quotient
+    // int r;     //   r is remainder
+    // // a = (b*q) +r
+    // r = a - (b*q);
+    // cout<<r;
+
+
+    //modulus operator (for find REMAINDER)
+
+    int p = 10;
+    int q = 3;
+    int s = p%q;
+    cout<<s;
+
+}
