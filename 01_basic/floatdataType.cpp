1a04134267fe351c3e27d009f527ceabdf2e9bb9
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-07 09:20:52 +0000

    Float datatype

---
+++ 01_basic/floatdataType.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x = 3.14;
+    x = x + 1; 
+    cout<<x;
+
+    cout<<5/2<<endl;
+    cout<<5.0/2<<endl;
+    cout<<5/2.0<<endl;
+    cout<<5.0/2.0<<endl;
+}
