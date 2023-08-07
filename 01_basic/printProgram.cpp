32ee2a6914204410cd0beec4c01f7d913e041073
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-07 09:21:39 +0000

    print Program

---
+++ 01_basic/printProgram.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hellow world"<<endl;
+    cout<<"namste bharat"<<endl;
+    cout<<"college wallah";
+
+    cout<<"hello cw"<<endl<<"hello pw";
+
+    cout<<"Physics wallah"<<endl;
+    cout<<"College wallah"<<endl;
+    cout<<"Jee wallah"<<endl;
+    cout<<"NCERT wallah"<<endl;
+
+    cout<<4+5<<endl;
+    cout<<"4+5"<<endl;
+
+    return 0;
+}
