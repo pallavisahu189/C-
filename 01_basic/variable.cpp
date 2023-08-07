91b472c0aa40d54b75c2ac4c193a22b09e90f1e2
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-07 09:21:59 +0000

    Variables

---
+++ 01_basic/variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 7;
+    cout<<x<<endl;
+    x = 10;
+    x = 19;
+    x = x + 10;
+    cout<<x<<endl;
+    x += 5;
+    cout<<x<<endl;
+
+}
