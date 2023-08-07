44c6ef9992b3fc2a826fec126f47859397c14690
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-07 09:18:22 +0000

    Calculate Area of Circle

---
+++ 01_basic/calculateAreaOfCircle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float radius = 5;
+    float pi = 3.1415;
+    float area = pi*radius*radius;
+    cout<<area<<endl;
+
+    //or
+
+    float r = 8;
+    float a = 3.1415;
+    a = 3.1415*r*r;
+    cout<<a<<endl;
+
+    //or
+
+    float r1 = 5.2;
+    cout<<3.1415*r1*r1<<endl;
+}
