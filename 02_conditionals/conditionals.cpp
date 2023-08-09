6704060dfda5044831f89654268c4441ec4cdbca
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-09 13:04:21 +0000

    character is a vowel or a consonant

---
+++ 02_conditionals/conditionals.cpp
@@ -0,0 +1,21 @@
+// Write a program to check whether a given character is a vowel or a consonant.
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter a character : ";
+    cin>>ch;
+    int ascii = (int) ch;
+    if((ascii>=97 && ascii<=122) || (ascii>=65 && ascii<=90)){
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            cout<<"The character is vowel";
+        }
+        else{
+            cout<<"The character is consonant";
+        }
+    }
+    else{
+        cout<<"The character is alphabet";
+    }
+}
