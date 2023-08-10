b1046687c7946c24a3b5a3583b09d762d42bbdcb
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-10 09:00:55 +0000

    sum of the two numbers

---
+++ 03_input/sumOfTwoNumbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x , y , sum ;
+    cout<<"Enter a first number : "<<endl;
+    cin>>x;
+    cout<<"Enter a second number : "<<endl;
+    cin>>y;
+    sum = x + y;
+    cout<<"Sum of the number that you gave is: "<<sum;
+}
