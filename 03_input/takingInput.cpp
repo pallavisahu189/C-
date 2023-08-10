97eea5bb6a39ddd4c5c07281fc24d215cf43d24e
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-10 09:01:37 +0000

    take input

---
+++ 03_input/takingInput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;  //x ko ek value dena hai
+    cout<<x<<endl;
+
+    //or
+
+    int y;
+    cout<<"Enter a number: ";
+    cin>>y;
+}
