d697cb440769480f5bed21b54333216e2a579595
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-10 09:00:02 +0000

    typecasting

---
+++ 03_input/typeCasting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){ //int , float , bool, char -> typecasting
+    float x = 7.1;
+    int y;
+    y = (int)x;
+    cout<<y;
+
+
+    bool p = true;
+    cout<<p+10;
+
+
+    char ch = 'a';
+    int s = (int)ch;
+    cout<<s;
+
+}
