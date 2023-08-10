2d32149817bab262ef99d5500cc9f486072b7f10
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-10 08:54:35 +0000

    area of circle

---
+++ 03_input/areaOfCircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int radius;
+    cout<<"Enter a radius: ";
+    cin >> radius;
+    float area = 3.1415 * radius * radius;
+    cout<<"The area is :  "<< area<<endl;
+}
