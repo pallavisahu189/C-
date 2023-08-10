f1ea13ec53a05b46e0385c12071192fe111191f3
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-10 08:57:25 +0000

    operators

---
+++ 03_input/operators.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 3;
+    cout<<(x==7);  // reletional operators hmesa 0 or 1 me answer deta hai
+    cout<<(x==3);
+    cout<<(x!=4);
+
+}
