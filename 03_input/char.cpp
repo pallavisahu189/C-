4598cd36a89078b06a698acfb6934117d7328b64
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-10 08:55:36 +0000

    check char

---
+++ 03_input/char.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch = 'a';
+    cout<<ch;
+    char x = 'q';
+    cout<<ch<<"  "<<x;
+
+    //type conversion
+
+    char x = 'A';
+    cout<<(int)x;                    // ASCII value A = 65
+                                    // ASCII value a = 97
+                                    // // ASCII value 0 = 48
+}
