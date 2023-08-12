bc5972fdc046fd434411c822df37cc2c915d528a
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-12 11:23:34 +0000

    Print Hello World 'n' times

---
+++ 05_loops/helloWorld.cpp
@@ -0,0 +1,13 @@
+// Print Hello World 'n' times. Take 'n' as
+// input from user in seperate lines.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cout<<"Hello World"<<endl;
+}
+}
