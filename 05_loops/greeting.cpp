72003d02b6c3c2952c4b97060ed878c386bc2319
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-12 11:22:59 +0000

    print 10 time good morning

---
+++ 05_loops/greeting.cpp
@@ -0,0 +1,11 @@
+// 10 time print good morning
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1; i<=10; i++){
+        cout<<"HELLO"<<endl;
+        cout<<"GOOD MORNING"<<endl;
+
+    }
+}
