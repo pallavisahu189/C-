7ed93b60596af99822b6122f956f150f89c5b531
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-13 10:16:07 +0000

    odd number 1 to 100

---
+++ 06_loops2/oddNumber1to100.cpp
@@ -0,0 +1,11 @@
+//WAP to print odd numbers from to 100
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1; i<=100; i++){
+        if(i%2!=0){
+            cout<<i<<" " ;
+        }
+    }
+}
