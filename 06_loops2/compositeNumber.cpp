c3aea2f4dc1631a0aef3e6bfb58fe4731615f570
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-13 10:14:06 +0000

    composite numbers

---
+++ 06_loops2/compositeNumber.cpp
@@ -0,0 +1,16 @@
+// composite Number - jo apne alava or bhi number se divisible ho
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    for(int i=2; i<n-1; i++){
+        if(n%i==0){
+            cout<<n<<" is a composite number";
+            break;
+        }
+        
+    }
+}
