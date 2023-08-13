cbebe5692db86b3112730fb0d62830110adb7ad5
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-13 10:14:28 +0000

    continue statement

---
+++ 06_loops2/continueStatement.cpp
@@ -0,0 +1,13 @@
+//WAP to print odd numbers from to 100
+
+// using continue statement
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1; i<=100; i++){
+        if(i%2!=0) continue;
+        cout<<i<<" " ;
+        
+    }
+}
