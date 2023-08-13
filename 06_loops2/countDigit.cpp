b7de45dbc561cfa4df132c48b7bb366f319452be
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-13 10:14:45 +0000

    count digit

---
+++ 06_loops2/countDigit.cpp
@@ -0,0 +1,17 @@
+//WAP to count digits of a given number.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a Number : ";
+    cin>>n;
+    int count = 0;
+    int a = n;
+    while(n>0){
+        n = n/10;
+        count = count + 1;    }
+
+        if(a==0) cout<<1;
+        else cout<<count;
+}
