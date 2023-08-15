a6835defdbc3553fcec0992bf4633e5e960b39e2
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-15 11:12:28 +0000

    students marks

---
+++ 08_ternaryAndSwitch /marks.cpp
@@ -0,0 +1,12 @@
+//Given the marks of the student. if the marks
+//are greater than 33 print the result as pass 
+// otherwish fail without using if else statement.
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter marks : ";
+cin>>n;
+(n>33) ? cout<<"student pass" : cout<<"student fail";
+}
