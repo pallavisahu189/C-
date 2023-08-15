da36c93f4b2e2d7b5ffb1d23a14c9aa97ede05d9
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-15 11:50:35 +0000

    syntax and declaration in array

---
+++ 13_array/syntaxAndDeclaration.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main (){
+    // int arr[6];
+    // arr[0] = 1;
+    // arr[1] = 2;
+    // arr[2] = 3;
+    // arr[3] = 4;
+    // arr[4] = 5;
+    // arr[5] = 6;
+    // cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<" "<<arr[3]<<" "<<arr[4]<<" "<<arr[5];
+
+
+    int arr[6] = {1,2,3,4,5,8};
+    //cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<" "<<arr[3]<<" "<<arr[4]<<" "<<arr[5];
+
+
+    for(int i=0; i<=5; i++){
+        cout<<arr[i]<<" ";
+    }
+}
