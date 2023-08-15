f9bdc1c387744c86b6edc814266f88bc2a9c657d
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-15 11:08:45 +0000

    day of week name using switch case

---
+++ 08_ternaryAndSwitch /weekDay.cpp
@@ -0,0 +1,42 @@
+// Write a program to input week number(1-7)
+// and print day of week name using switch case.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter day number : ";
+    cin>>x;
+    switch(x){
+        case 1:
+        cout<<"Monday";
+        break;
+
+        case 2:
+        cout<<"Tuesday";
+        break;
+
+        case 3:
+        cout<<"Wednesday";
+        break;
+
+        case 4:
+        cout<<"Trusday";
+        break;
+
+        case 5:
+        cout<<"Friday";
+        break;
+
+        case 6:
+        cout<<"Saturday";
+        break;
+
+        case 7:
+        cout<<"sunday";
+        break;
+
+        default:
+        cout<<"this is not day";
+    }
+}
