78b457351f02307319316812d259b5111c34fa69
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-16 10:46:32 +0000

    odd number triangle

---
+++ 09_patternPrinting-1/oddNumTriangle.cpp
@@ -0,0 +1,21 @@
+//1
+//1 3 
+//1 3 5
+//1 3 5 7
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    
+    for(int i=1; i<=n; i++){
+        int a=1;
+        for(int j=1; j<=i; j++){
+        cout<<a;
+        a+=2;
+        }
+        cout<<endl;
+    }
+}
