540c101edb2ba63627d572d800e538fd73a781da
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-17 11:43:59 +0000

    Linear search

--- 13_array/linearSearch.cpp
+++ 13_array/linearSearch.cpp
@@ -17,8 +17,11 @@ int main(){
     cout<<"Enter the element you want to search : ";
     cin>>x;
     //search
-
+    // check mark
+    bool flag = false; //false = not present
     for(int i=0; i<=n-1; i++){
-        if(arr[i]==x) cout<<"Present";
+        if(arr[i]==x) flag = true;
     }
+    if(flag==true) cout<<"Element found";
+    else cout<<"Element not found";
 }
