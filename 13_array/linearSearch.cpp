e6d6ad64465820b528ffeabd90c3b89c5f5e109b
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-17 11:26:57 +0000

    Linear search

---
+++ 13_array/linearSearch.cpp
@@ -0,0 +1,24 @@
+// Linear search
+// Ques : Find the element x in the array . Take array and
+// x as input.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+    int arr[n];
+    //input
+    for(int i=0; i<=n-1; i++){
+        cin>>arr[i];
+    }
+    int x;
+    cout<<"Enter the element you want to search : ";
+    cin>>x;
+    //search
+
+    for(int i=0; i<=n-1; i++){
+        if(arr[i]==x) cout<<"Present";
+    }
+}
