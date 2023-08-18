edc4d37c35d51c139a9b1b217349d2f4eb94d6cc
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-18 11:28:56 +0000

    star pyramid

---
+++ 10_patternPrinting-2/starPyramid.cpp
@@ -0,0 +1,24 @@
+// *Ques : Print the given pattern
+
+//    * 
+//   *** 
+//  ***** 
+// *******
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter row number : ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=2*i-1; j++){
+            cout<<"*";
+        
+        }
+        cout<<endl;
+    }
+}
