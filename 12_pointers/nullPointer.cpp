517f5b7ad1f422af901dac88db325290eb9ce823
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-08-20 11:17:28 +0000

    null pointer

--- 12_pointers/nullPointer.cpp
+++ 12_pointers/nullPointer.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int main(){
     int x;
+   
     int* ptr = NULL;
-    cout<<&ptr;
+    cout<<&ptr<<endl;
+    
+
+     int* ptr1 = 0 ;
+     cout<<&ptr1<<endl;
+
+    //  char* ptr2 = '\0';
+    //  cout<<&ptr2<<endl;
 }
