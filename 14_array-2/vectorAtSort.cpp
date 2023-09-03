f41cf88357c3beb50e9263307f6de3c811187718
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-09-03 12:20:15 +0000

    vector at

---
+++ 14_array-2/vectorAtSort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v;
+    v.push_back(9);
+    v.push_back(8);
+    v.push_back(7);
+    v.push_back(6);
+    v.push_back(5);
+
+    v.at(2)=11;
+    cout<<v.at(2);       /// cout<<v[2];
+    cout<<endl;           // cout<<v.at(2);  dono hi index ke element ko print krate hai
+
+    for(int i=0; i<v.size(); i++){
+        cout<<v.at(i)<<" ";
+    }
+
+
+
+}
