23358afa096760b93bae76c640876ba6952f93de
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-09-09 09:24:48 +0000

    reverse order

---
+++ 14_array-2/reverseArray.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void display(vector<int>&a){
+    for(int i=0; i<a.size(); i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    vector<int>v;
+    v.push_back(8);
+    v.push_back(5);
+    v.push_back(9);
+    v.push_back(6);
+    v.push_back(4);
+    v.push_back(2);
+
+    display(v);
+    //reverse
+
+    int i = 0;
+    int j = v.size()-1;
+
+    while(i<=j){
+        //swap v[i] and v[j]
+        int temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+
+        i++;
+        j--;
+    }
+    display(v);
+}
