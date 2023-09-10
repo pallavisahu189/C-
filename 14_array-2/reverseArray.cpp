a4742a4861add1816ed714dfa7247a87136edbae
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-09-10 07:08:19 +0000

    reverse Array

--- 14_array-2/reverseArray.cpp
+++ 14_array-2/reverseArray.cpp
@@ -33,5 +33,11 @@ int main(){
         i++;
         j--;
     }
+
+    // for(int i=0; j=v.size()-1; i<=j; i++,j--){
+    //     int temp = v[i];
+    //     v[i] = v[j];
+    //     v[j] = temp;
+    // }
     display(v);
 }
