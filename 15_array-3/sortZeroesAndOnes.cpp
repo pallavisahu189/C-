43d7790e6a78e74bea17d3f3fd818fecf49befdc
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-09-11 09:06:16 +0000

    sort zero's and one's

---
+++ 15_array-3/sortZeroesAndOnes.cpp
@@ -0,0 +1,46 @@
+//sort the array of zero's and one's
+
+#include<iostream>
+#include<vector>
+using namespace std;
+void sort01 (vector<int>&v){
+    int n = v.size();
+    int nofones = 0;
+    int nofzeros = 0;
+
+    for(int i=0; i<n; i++){
+        if(v[i]==0) nofzeros++;
+        else nofones++;
+    }
+
+    //filling elements
+
+    for(int i=0; i<n; i++){
+        if(i<nofzeros) v[i]=0;
+        else v[i] = 1;
+    }
+}
+int main(){
+    vector<int>v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(1);
+
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i];
+    }
+    cout<<endl;
+
+    sort01();
+
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i];
+    }
+    cout<<endl;
+
+}
