4523d9859dfd93fe09afe265c6f36d13dd840a25
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-09-11 09:14:28 +0000

    sort the array of 0's and 1's M-2

---
+++ 15_array-3/sortZeroesAndOnesM_2.cpp
@@ -0,0 +1,47 @@
+//sort the array of zero's and one's
+
+#include<iostream>
+#include<vector>
+using namespace std;
+void sort01m2 (vector<int>&v){
+    int n = v.size();
+    int i = 0;
+    int j = n-1;
+
+    while(i<j){
+        if(v[i]==0)  i++;
+        else if(v[j]==1) j--;
+        //if(i>j) break;
+        else if(v[i]==1 && v[j]==0){
+            v[i] = 0;
+            v[j] = 1;
+            i++;
+            j--;
+        }
+    }
+   
+}
+int main(){
+    vector<int>v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(0);
+    v.push_back(1);
+
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i];
+    }
+    cout<<endl;
+
+    sort01m2();
+
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i];
+    }
+    cout<<endl;
+
+}
