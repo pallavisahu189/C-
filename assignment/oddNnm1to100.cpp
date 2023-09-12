a38a2fa4e81daa2c449cf4a114ff777b5dd845fa
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-09-12 17:27:30 +0000

    odd numbers 1 to 100

---
+++ assignment/oddNnm1to100.cpp
@@ -0,0 +1,12 @@
+//1 to 100 odd number
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1; i<=100; i++){
+        if(i%2!=0){
+            cout<<i<<" ";
+        }
+    }
+
+}
