d63aa5a4a7238c70e793cc27310bd14556beb636
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-09-22 09:58:35 +0000

    declaration of 2D array

--- 16_TwoDarrays-1/declaration.cpp
+++ 16_TwoDarrays-1/declaration.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[3][3];
-    //row ->3   0-2
-    //column ->   0-2
-    cout<<arr[0][0]<<" ";
+    // int arr[3][3];
+    // //row ->3   0-2
+    // //column ->   0-2
+    // cout<<arr[0][0]<<" ";
 
-    arr[0][0] = 6;
-    cout<<arr[0][0];
+    // arr[0][0] = 6;
+    // cout<<arr[0][0];
+
+
+    // ya
+
+    int arr[3][3] = {{1,2,3,},{4,5,6},{7,8,9}};
+    cout<<arr[0][2]<<endl;  
+
+    int arr1[][3] = {{1,2,3,},{4,5,6},{7,8,9}};
+    cout<<arr1[0][2]<<endl; 
+
+    // int arr2[3][] = {{1,2,3,},{4,5,6},{7,8,9}};     //column dena jruri hai nhi to code me error aata h
+    // cout<<arr2[0][2]<<endl; 
+    
 }
