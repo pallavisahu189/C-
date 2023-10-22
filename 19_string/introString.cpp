4ed78bbe29499805596b9b2cde5314128ebe84f4
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-10-22 07:16:47 +0000

    basic intro of string

--- 19_string/introString.cpp
+++ 19_string/introString.cpp
@@ -8,9 +8,21 @@ int main(){
     for(int i=0; i<5; i++){
         cout<<str[i]<<" ";
     }
+    cout<<(int)(str[5]);
+
     cout<<"abc\tdef"<<endl;   // \t = tap(4 space)
 
     cout<<"abc\ndef"<<endl;   //\n = next line
 
     cout<<"abc\0def";       // null charactor = 0  (\0 ko dekhne se machine aage ke sttring ko print nhi krta h)
+
+    char ch = '\0';
+    cout<<ch; 
+
+    // agr hmko string ka size nhi pta h to kaise print krna hai................>>>>>
+
+    char s[] = {'a','b','c','d','e','f'};for(int i=0; s[i]!='\0'; i++){
+        cout<<s[i]<<" ";
+    }
+
 }
