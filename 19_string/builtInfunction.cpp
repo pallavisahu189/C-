146bd4f33bf7cf3186cbc48c8dfd58f3f41b205f
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-10-23 15:46:14 +0000

    built in function size and lenght

---
+++ 19_string/builtInfunction.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str = "I am pallavi , i am good and smart girl";
+    cout<<str.size()<<endl;
+    int len = str.length();
+    cout<<len;
+}
