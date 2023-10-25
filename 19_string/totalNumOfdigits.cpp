24547753c209fb713eae4f993d73a10142b23113
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-10-25 08:50:09 +0000

    return the total number of digits in a number without using any loop.

---
+++ 19_string/totalNumOfdigits.cpp
@@ -0,0 +1,10 @@
+//return the total number of digits in a number without using any loop.
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int x = 63489 ;
+    string s = to_string(x);
+    cout<<s.length();
+}
