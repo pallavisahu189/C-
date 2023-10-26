72db43c183a2b014a91e352339e83ffebc2d76ba
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-10-26 09:45:30 +0000

    Given two string s and t, return true if t is an anagram of s, and false otherwise

---
+++ 20_string-2/anagram.cpp
@@ -0,0 +1,14 @@
+// Given two string s and t, return true if t is an anagram of s, and false otherwise
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s="physicswallah";
+    string t="phywallahsics" ;
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    if(s==t)cout<<"true";
+    else  cout<<"false";
+}
