73317ee73a5cf7eb8b6611a2bc74f8b6daf4ccfb
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-11-01 11:34:43 +0000

    reverse the second half of the string.

---
+++ all assignment que./string/reversetheSecondHalfOf TheString.cpp
@@ -0,0 +1,19 @@
+// Input a string of even length and reverse the second half of the string.
+// Input : str = "abcdefgh"
+// Output : abcdhgfe
+// Input :str = "pwians"
+// Output : pwisna
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if(s.size()%2 != 0) cout<<"Invalid input."<<endl;
+    else{
+        reverse(s.begin() + n / 2, s.end());
+        cout << s;
+    }
+}
