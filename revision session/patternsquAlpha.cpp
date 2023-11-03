a60c52bd323f16a6d7c80fa48028375b34741363
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-11-03 12:02:58 +0000

    patteren square alphabats

---
+++ revision session/patternsquAlpha.cpp
@@ -0,0 +1,18 @@
+//ABCD
+//ABCD
+//ABCD
+//ABCD
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter row number : ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cout<<(char)(j+64)<<" ";
+        }
+        cout<<"\n";
+    }
+}
