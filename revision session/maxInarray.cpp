51448ea614e26e44d31475f3cbfc78ea57547785
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-11-07 16:46:56 +0000

    maximum in array

---
+++ revision session/maxInarray.cpp
@@ -0,0 +1,23 @@
+// Ques : Find the maximum value out of all the elements
+// in the array.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+    int arr[n];
+    //input
+    for(int i=0; i<=n-1; i++){
+        cin>>arr[i];
+    }
+
+    int max = arr[0];
+    for(int i=1; i<=n-1; i++){
+        if(max<arr[i]) max = arr[i];
+    }
+    cout<<max;
+}
+
+    
