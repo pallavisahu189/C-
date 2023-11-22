a28c2d170742cfa996529e2a49cd9b055d2d893b
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-11-22 11:17:42 +0000

    find the Length of array

---
+++ revision session/sizeOfarr.cpp
@@ -0,0 +1,9 @@
+//find the Length of array?
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {2,3,1,4,5,6,98,7,5,54,6,541,4541,2215,458,475};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout<<size;
+}
